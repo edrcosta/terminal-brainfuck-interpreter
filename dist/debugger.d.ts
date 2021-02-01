0243c79fc3ed7bf9e9410672972a7db15f500ecc
Eder Costa <edercosta@Eders-iMac.local>
2021-01-31 23:18:52 -0300

    fix

--- dist/debugger.d.ts
+++
@@ -1,4 +0,0 @@
-import { Computer } from './computer';
-export declare class Debugger {
-    static debugg(computer: Computer): false | undefined;
-}
--- dist/interpreter.d.ts
+++
@@ -1,5 +0,0 @@
-import { iOperation, iInstructionTable } from './interfaces';
-export declare class InterpreterLang {
-    static instructionTable: iInstructionTable<Function>;
-    static getExec: (OPCODE: any) => iOperation;
-}
